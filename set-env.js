33b6990a6c9cfe6c284aeafcfd962b289e88a9f5
NicolaIsGaray <nicogaray2713@gmail.com>
2026-05-27 19:12:05 -0300

    Fix: Static Files

--- set-env.js
+++ set-env.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
-const targetPath = './src/environments/environment.ts';
+const targetPath = './src/environments/environment.prod.ts';
 
 const envConfigFile = `
 export const environment = {
   production: true,
-  apiUrl: '${process.env.ANGULAR_API_URL || 'https://cursitu-api.onrender.com/api'}'
+  api: '${process.env.ANGULAR_API_URL || 'https://cursituv2-demo.onrender.com/api'}',
+  apiUrl: '${process.env.ANGULAR_API_URL || 'https://cursituv2-demo.onrender.com/api'}'
 };
 `;
 
 fs.writeFile(targetPath, envConfigFile, function (err) {
-   if (err) {
-       throw console.error(err);
-   }
-   console.log(`Environment generado correctamente en ${targetPath}`);
+  if (err) {
+    throw console.error(err);
+  }
+  console.log(`Environment generado correctamente en ${targetPath}`);
 });
